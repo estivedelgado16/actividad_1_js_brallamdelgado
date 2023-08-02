98e6fd4b5ecfdd090009d0a3128ebd89b656e190
brallam351 <brallamdelgado351@gmail.com>
2023-08-01 19:43:55 -0500

    CLASE 5

---
+++ index.js
@@ -0,0 +1,10 @@
+let name = "brallam"
+let last_nam = ("delgado")
+const year_birth = ("2001")
+let city = ("medellin")
+
+console.log("Hello","my name is ", name, last_nam,", i live in",city,"and, i am 22 year old", year_birth)
+
+
+
+
